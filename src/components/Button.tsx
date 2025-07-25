cd9f4fcb8fbee9bf567f3e747c1f7e11f6d01f5d
Developer <honeysharmaemail@gmail.com>
2025-07-25 23:57:23 +0530

    Refactor and improved button reusability - fix: Handled undefined when className props is undefined. - feat: Add button variants for quick usable styled button. - fix: Make color transition smooth on hover. - fix: Improve accessibility by adding aria-busy - feat: Add rest operator to accept any global attribute as props in the button. - feat: Add disabled support with visual indication.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,17 +6,41 @@ interface ButtonProps {
   iconPath?: string;
   onClick?: () => void;
   isLoading?: boolean;
+  variant?: ButtonVariant;
+  disabled?: boolean;
 }
 
+type ButtonVariant = "primary" | "secondary" | "danger";
+
+const variantClasses: Record<ButtonVariant, string> = {
+  primary: "bg-gray-500 hover:bg-gray-600 text-white",
+  secondary: "bg-black hover:bg-gray-800 text-white",
+  danger: "bg-red-500 hover:bg-red-600 text-white",
+};
+
 export const Button = (props: ButtonProps) => {
-  const { children, className, onClick, isLoading, iconPath } = props;
+  const {
+    children,
+    className,
+    onClick,
+    isLoading,
+    iconPath,
+    variant = "primary",
+    disabled,
+    ...rest
+  } = props;
 
   return (
     <button
+      {...rest}
+      disabled={disabled}
       onClick={onClick}
-      className={`bg-blue-500 py-2 px-4  rounded-md min-w-18 cursor-pointer hover:brightness-90 focus:outline-gray-400 focus:shadow ${className}`}
+      aria-busy={isLoading}
+      className={`py-2 px-4 rounded-md min-w-18 focus:outline-gray-400 shadow transition-colors duration-300 ease-in-out ${
+        disabled ? "opacity-50 cursor-not-allowed" : " cursor-pointer"
+      } ${variantClasses[variant]} ${className ?? ""}`}
     >
-      <span className="flex flex-row items-center  justify-center gap-2">
+      <span className="flex flex-row items-center justify-center gap-2">
         {isLoading ? (
           <InlineLoader />
         ) : (
